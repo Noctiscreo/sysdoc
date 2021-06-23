1e428adf5320d0300d9dc4e2ad04184a034c1b3e
John Hasznosi <54283261+Noctiscreo@users.noreply.github.com>
2021-06-23 01:14:40 +0100

    Updated 'var' to 'let'.

--- sysdoc.js
+++ sysdoc.js
@@ -1,4 +1,4 @@
-var dates = [
+let dates = [
 {
   name: "Christmas",
   date: new Date(2021,12,25)
@@ -15,12 +15,14 @@ var dates = [
 }
 ];
 
-// Loops through the 'dates' array and calculates the number of days left from the current date.
-$("#trigger").click(function(){
-  var today = new Date();
-  for (var i = 0; i < dates.length; i++) {
-  var daysToCustomDate = parseInt((dates[i].date-today)/(24*3600*1000)); 
-
+// Loops through the 'dates' objects array and calculates the number of days left from current date.
+function calculate() {
+  let today = new Date();
+  for (let i = 0; i < dates.length; i++) {
+  let daysToCustomDate = parseInt((dates[i].date-today)/(24*3600*1000)); 
+    
 // Displays the name of the date and the number of days left until that date.
   $("#days-until").append("<li>" + daysToCustomDate.toString()+" days until " + dates[i].name + "</li>")};
-})
+};
+
+$("#trigger").click(calculate());
